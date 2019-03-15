8d7f68c219ff590baeba953c81cfee525b117356
selinatesfa <selina.tesfabrhan@gmail.com>
2019-03-15 12:19:18 -0500

    startup waagreen

---
+++ waagreen/src/main/java/waa/green/WaagreenApplication.java
@@ -0,0 +1,13 @@
+package waa.green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaagreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaagreenApplication.class, args);
+	}
+
+}
