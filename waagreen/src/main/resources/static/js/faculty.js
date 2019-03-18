6e1e963fc53721e48072c128c2fd3e930606089f
selinatesfa <selina.tesfabrhan@gmail.com>
2019-03-18 11:45:34 -0500

    update models

---
+++ waagreen/src/main/resources/static/js/faculty.js
@@ -0,0 +1,42 @@
+// pathname is contextRoot/path/path/ so we want to get [1] 
+// that is where contextRoot is
+// [0] has "" as a result of split 
+var contextRoot = "/" + window.location.pathname.split('/')[1];
+
+function serializeObject(form) {
+	var jsonObject = {};
+	var array = form.serializeArray();
+	$.each(array, function() {
+		jsonObject[this.name] = this.value;
+	});
+	return jsonObject;
+};
+$( document ).ready(function() {
+	$('#result').hide();
+	
+	$('#submitBtn').click(function(event){
+		event.preventDefault();
+				//var dataToSend = JSON.stringify(serializeObject($('#categoryForm')));
+		$.ajax({
+			type : 'GET',
+			url : contextRoot + '/addCategory',
+			dataType : "json", // Accept header
+			//data : dataToSend,
+			contentType : 'application/json', // Sends - Content-type
+			success : function(response) {
+					$("#result").append();
+				
+				$('#result').show();
+			},
+			error : function(errorObject) {
+										
+					alert(errorObject.responseJSON.errors(0)); 
+					
+				}
+			}
+		});
+		
+	});  
+});
+
+
