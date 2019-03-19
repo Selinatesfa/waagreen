06b9dac9832d72cc77a9058c3f233ef62e9deb4b
Semuredda <44593335+Semuredda@users.noreply.github.com>
2019-03-19 15:19:47 -0400

    I changed the Studentcontroller name to StudentController. Camel case.

--- waagreen/src/main/java/waa/green/controller/Studentcontroller.java
+++ waagreen/src/main/java/waa/green/controller/Studentcontroller.java
@@ -5,26 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.ui.Model;
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import waa.green.model.Block;
 import waa.green.model.Student;
 import waa.green.service.BlockService;
 import waa.green.service.StudentService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Controller
 @RequestMapping("/student")
-public class Studentcontroller {
+public class StudentController {
 
-    private static final Log logger = LogFactory.getLog(Studentcontroller.class);
+    private static final Log logger = LogFactory.getLog(StudentController.class);
 
     @Autowired
     private StudentService studentService;
@@ -45,7 +41,7 @@ public class Studentcontroller {
 //        return "redirect:student/StudentDetails";
 //    }
 
-    @GetMapping("/StudentDetails")
+    @GetMapping("/student")
     public String showForm(){
 
         return "student/StudentDetails";
