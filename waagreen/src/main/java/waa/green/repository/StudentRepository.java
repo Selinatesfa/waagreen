b2a19784a750a11669fe0154435545e84a33c2e1
Efremsd <efremdebesay25@gmail.com>
2019-03-18 21:43:45 -0500

    ecommit

---
+++ waagreen/src/main/java/waa/green/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package waa.green.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import waa.green.model.Student;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student,Long> {
+    @Query(value = "select a.dateOfEntry from Student a ")
+    public List<Date> findallDateOfEntry();
+}
