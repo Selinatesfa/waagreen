3e6c86d71650da333a6058c6bb64177622e24d8f
Efremsd <efremdebesay25@gmail.com>
2019-03-18 19:50:54 -0500

    ecommit

--- waagreen/src/main/java/waa/green/repository/AttendanceRepository.java
+++ waagreen/src/main/java/waa/green/repository/AttendanceRepository.java
@@ -3,14 +3,19 @@ package waa.green.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import waa.green.model.Attendance;
 import waa.green.model.Block;
 
+import java.util.Date;
+
 @Repository
 public interface AttendanceRepository extends CrudRepository<Attendance, Long> {
 	@Query(value="select a from Attendance a where :course MEMBER OF a.students.courses and a.block=:block")
 	public List<Attendance> generatereportbycourseandblock(String course, String block);
-	
+
+    @Query(value="")
+    public Attendance generateReportByEntry(Date dateOfEntry);
 }
