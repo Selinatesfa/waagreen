0e944f9027669fa965699723b985d7ff9aa56d8f
Enkhbaatar <enhee.day@gmail.com>
2019-03-18 21:26:19 -0500

    AttendanceRepository, AttendanceTypeRepository

--- waagreen/src/main/java/waa/green/model/Attendance.java
+++ waagreen/src/main/java/waa/green/model/Attendance.java
@@ -19,7 +19,7 @@ import java.util.Set;
 @Entity
 public class Attendance {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "attend_id")
     private long id;
     @Column( name = "attendance_date")
