2f0a509c7ef3136064b631ed2554d9814812af78
Semuredda <44593335+Semuredda@users.noreply.github.com>
2019-03-19 12:34:50 -0400

    1

--- waagreen/src/main/java/waa/green/repository/StudentRepository.java
+++ waagreen/src/main/java/waa/green/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package waa.green.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+<<<<<<< Updated upstream
 import org.springframework.stereotype.Repository;
 import waa.green.model.Student;
 
@@ -12,4 +13,19 @@ import java.util.List;
 public interface StudentRepository extends CrudRepository<Student,Long> {
     @Query(value = "select a.student.dateOfEntry from Attendance a ")
     public List<Date> findallDateOfEntry();
+=======
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import waa.green.model.Attendance;
+import waa.green.model.Student;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+    @Query("SELECT stu FROM Student stu WHERE stu.id =:id")
+    public Attendance attendanceInfoAccumulative(@Param("id") Long id);
+
+
+
+>>>>>>> Stashed changes
 }
