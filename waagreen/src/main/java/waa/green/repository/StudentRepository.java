130de7717903a464e13dad4ba2a8dac1bc04d4b6
Semuredda <44593335+Semuredda@users.noreply.github.com>
2019-03-19 12:38:03 -0400

    I added some attributes to the student class and annotate the service and repository class.

--- waagreen/src/main/java/waa/green/repository/StudentRepository.java
+++ waagreen/src/main/java/waa/green/repository/StudentRepository.java
@@ -2,8 +2,9 @@ package waa.green.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-<<<<<<< Updated upstream
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import waa.green.model.Attendance;
 import waa.green.model.Student;
 
 import java.util.Date;
@@ -13,19 +14,7 @@ import java.util.List;
 public interface StudentRepository extends CrudRepository<Student,Long> {
     @Query(value = "select a.student.dateOfEntry from Attendance a ")
     public List<Date> findallDateOfEntry();
-=======
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import waa.green.model.Attendance;
-import waa.green.model.Student;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
 
     @Query("SELECT stu FROM Student stu WHERE stu.id =:id")
-    public Attendance attendanceInfoAccumulative(@Param("id") Long id);
-
-
-
->>>>>>> Stashed changes
+    Attendance attendanceInfoAccumulative(@Param("id") Long id);
 }
