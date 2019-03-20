2fa77d6a0bc846fde8c680b7fbd8b1dcfdd3021a
Efremsd <efremdebesay25@gmail.com>
2019-03-19 22:45:45 -0500

    eecomkfkj

--- waagreen/src/main/java/waa/green/service/impl/AttendanceServiceImpl.java
+++ waagreen/src/main/java/waa/green/service/impl/AttendanceServiceImpl.java
@@ -1,8 +1,20 @@
 package waa.green.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import waa.green.model.Attendance;
+import waa.green.model.EntryReportData;
+import waa.green.repository.AttendanceRepository;
 import waa.green.service.AttendanceService;
+
+import java.util.List;
 
 @Service
 public class AttendanceServiceImpl implements AttendanceService {
+    @Autowired
+    AttendanceRepository attendanceRepository;
+    @Override
+    public List<Attendance> findAttendancesByEntry(long id) {
+        return attendanceRepository.generateReportByEntry(id);
+    }
 }
