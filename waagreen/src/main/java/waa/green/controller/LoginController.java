bcf88bb03af86d07e979d537e94bbf39dc1e60c4
Enkhbaatar <enhee.day@gmail.com>
2019-03-21 09:56:29 -0500

    AttendanceRepository, AttendanceTypeRepository

--- waagreen/src/main/java/waa/green/controller/LoginController.java
+++ waagreen/src/main/java/waa/green/controller/LoginController.java
@@ -1,32 +1,12 @@
 package waa.green.controller;
 
-import java.util.HashSet;
-import java.util.Set;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;;
-import waa.green.model.Role;
-import waa.green.model.User;
-import waa.green.service.RoleService;
-import waa.green.service.UserService;
-
+import org.springframework.web.bind.annotation.*;
 @Controller
 public class LoginController {
-    @Autowired
-    UserService userService;
-    @Autowired
-    RoleService roleservice;
-
     @GetMapping("/login")
     public String login(@RequestParam(value = "error", required = false) String error, Model model) {
-        Role r = new Role();
-        r.setRole("FACULTY");
-        roleservice.saverole(r);
-        Set<Role> rolelist = new HashSet<>();
-        rolelist.add(r);
-        User user = new User("Lenny@mum.edu", "123456", "ss", "sss", 1, rolelist);
-        userService.saveUser(user);
         model.addAttribute("error", error);
         return "login";
     }
