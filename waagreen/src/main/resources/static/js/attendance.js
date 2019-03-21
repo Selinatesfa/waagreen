3361d08068a328d4ab3ef6b4895d7fa5fac67f57
Enkhbaatar <enhee.day@gmail.com>
2019-03-21 01:43:25 -0500

    AttendanceRepository, AttendanceTypeRepository

--- waagreen/src/main/resources/static/js/attendance.js
+++ waagreen/src/main/resources/static/js/attendance.js
@@ -58,6 +58,7 @@
 
         function error(xhr, status, exception) {
             var errorJson = xhr.responseJSON;
+            console.log(errorJson);
             var error = $('#fileError');
             error.html('');
             if (errorJson.errorType == "ValidationError") {
@@ -66,8 +67,8 @@
                 $.each(errorList, function (i, error) {
                     errMsg += (error.message + '<br />');
                 });
-                error.html('');
-                error.html(errMsg)
+                error.text('');
+                error.text(errMsg);
             }
         }
     }
